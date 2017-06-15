856984dedbb5bc942c817d4dba05a76f5798c18e
Marc Majcher <majcher@gmail.com>
2017-06-15 16:24:42 -0500

    working on dungeon

--- src/data/dungeons.js
+++ src/data/dungeons.js
@@ -7,39 +7,161 @@ module.exports = {
 };
 
 const size = {
+  // *total common and unique
+  // size/themes/areas
+  // 1-3 sm. 2/1d4 6/1d6+2
+  // 4-9 md. 3/1d6
+  // 10-11 lg. 4/1d6+1
+  // 12/2d6+4 16/3d6+6 24/4d6+10
+  // 12 huge 5/1d6+2
+};
 
+const builder = [
+  'aliens/precursors',
+  'demigod/demon',
+  'natural (caves, etc.)',
+  'natural (caves, etc.)',
+  'religious order/cult',
+  'Humanoid (p49)',
+  'Humanoid (p49)',
+  'dwarves',
+  'gnomes',
+  'elves',
+  'wizard/madman',
+  'monarch/warlord',
+];
+
+const purpose = [
+  'source/portal',
+  'mine',
+  'tomb/crypt',
+  'tomb/crypt',
+  'prison',
+  'lair/den/hideout',
+  'lair/den/hideout',
+  'stronghold/sanctuary',
+  'stronghold/sanctuary',
+  'shrine/temple/oracle',
+  'archive/library',
+  'unknown/mystery',
+];
+
+const ruination = [
+  'arcane disaster',
+  'damnation/curse',
+  'earthquake/fire/flood',
+  'earthquake/fire/flood',
+  'plague/famine/drought',
+  'plague/famine/drought',
+  'overrun by monsters',
+  'overrun by monsters',
+  'war/invasion',
+  'war/invasion',
+  'depleted resources',
+  'better prospects elsewhere',
+];
+
+const trap = [
+  'alarm',
+  'ensnaring/paralyzing',
+  'pit',
+  'crushing',
+  'piercing/puncturing',
+  'chopping/slashing',
+  'confusing (maze, etc.)',
+  'gas (poison, etc.)',
+  'Element (p50)',
+  'ambush',
+  'magical',
+  'roll twice'
+];
+
+const creature = [
+  'waiting in ambush',
+  'fighting/squabbling',
+  'prowling/on patrol',
+  'looking for food',
+  'eating/resting',
+  'guarding',
+  'on the move',
+  'searching/scavenging',
+  'returning to den',
+  'making plans',
+  'sleeping',
+  'dying',
+];
+
+const entity = [
+  'alien interloper',
+  'vermin lord',
+  'criminal mastermind',
+  'warlord',
+  'high priest',
+  'oracle',
+  'wizard/witch/alchemist',
+  'Monster lord(p49)',
+  'evil spirit/evil ghost',
+  'undead lord(lich, etc.)',
+  'demon',
+  'dark god',
+];
+
+const danger = {
+  table: [{
+    range: 4,
+    table: trap
+  }, {
+    range: 11,
+    table: creature
+  }, {
+    range: 12,
+    table: entity
+  }]
+};
+
+const dressing = [
+
+  // 1 junk / debris 1 2 tracks / marks 2 3 signs of battle 3 4 writing / carving
+  // 4 5 warning 5 6 dead Creature(p49) 6 7 bones / remains 7 8 book / scroll / map
+  // 8 9 broken door / wall 9 10 breeze / wind / smell 10 11 lichen / moss / fungus
+  // 11 12 Oddity(p50) 12
+];
+
+const feature = [
+  // cave - in /collapse 1 pit/shaft / chasm 2 pillars / columns 3 locked door / gate
+  // 4 alcoves / niches 5 bridge / stairs / ramp 6 fou nt ai n / well / p o ol 7 puzzle
+  // 8 altar / dais / platform 9 statue / idol 10 magic pool / statue / idol
+  // 11 connection to another 12 dungeon
+];
+
+const find = [
+  // trinkets
+  // tools weapons / armor supplies / trade goods coins / gems / jewelry poisons / potions
+  // adventurer / captive magic item scroll / book
+  // magic weapon / armor artifact
+  // roll twice
+
+];
+
+const discovery = {
+  table: [{
+    range: 3,
+    table: dressing
+  }, {
+    range: 9,
+    table: feature
+  }, {
+    range: 12,
+    table: find
+  }]
 };
 
 /*
 
-Dungeons
-Roll to determine different details as needed.
-                Dungeon Size
-Dungeon Foundation . Who built it, to what end?
 
-Size Themes Areas* ......................................................
-Builder Function
-1-3 sm. 2/1d4 6/1d6+2
- 1 aliens/precursors 1 source/portal
-2 demigod/demon 2 mine
-3-4 natural (caves, etc.) 3-4 tomb/crypt
-5 religious order/cult 5 prison
-6-7 Humanoid (p49) 6-7 lair/den/hideout
-8-9 dwarves/gnomes 8-9 stronghold/sanctuary 10 elves 10 shrine/temple/oracle
-11 wizard/madman 11 archive/library
-12 monarch/warlord 12 unknown/mystery
-4-9 md. 3/1d6
-10-11 lg. 4/1d6+1
-12/2d6+4 16/3d6+6 24/4d6+10
-12 huge 5/1d6+2 ......................................................
-*total common and unique
-Dungeon Ruination
------------------------------------------ 1 arcane disaster
-2 damnation/curse
-3-4 earthquake/fire/flood 5-6 plague/famine/drought 7-8 overrun by monsters
-9-10 war/invasion
-11 depleted resources
-12 better prospects elsewhere
+
+
+
           Dungeon Theme . What’s it all about? Choose or roll according to Dungeon Size.
           1-5 Mundane 6-9 Un usua l 10-12 Extraordinary
 1 rot/decay 1 2 torture/agony 2 3 madness 3 4 all is lost 4 5 noble sacrifice
@@ -57,33 +179,8 @@ the unquiet dead bottomless hunger incredible power unspeakable horrors holy war
 Plumb the Depths
 Dungeon Discovery . A starting point: extrapolate, embellish, integrate.
 
-                  1-3 Dressing 4-9 Feature 10-12Find
-1 junk/debris 1 2 tracks/marks 2 3 signs of battle 3 4 writing/carving
- 4 5 warning 5 6 dead Creature (p49) 6 7 bones/remains 7 8 book/scroll/map
-  8 9 broken door/wall 9 10 breeze/wind/smell 10 11 lichen/moss/fungus
-  11 12 Oddity (p50) 12
-cave-in/collapse 1 pit/shaft/chasm 2 pillars/columns 3 locked door/gate
-4 alcoves/niches 5 bridge/stairs/ramp 6 fou nt ai n/well/p o ol 7 puzzle
- 8 altar/dais/platform 9 statue/idol 10 magic pool/statue/idol
- 11 connection to another 12 dungeon
-trinkets
-tools weapons/armor supplies/trade goods coins/gems/jewelry poisons/potions
-adventurer/captive magic item scroll/book
-magic weapon/armor artifact
-roll twice
 
-Dungeon Danger . If they would notice, show signs of an approaching threat.
-         1-4 Trap 5-11 Creature (p49) 12 Entity
 
-1 alarm 1 2 ensnaring/paralyzing 2 3 pit 3 4 crushing 4 5 piercing/puncturing
- 5 6 chopping/slashing 6 7 confusing (maze, etc.) 7 8 gas (poison, etc.)
-  8 9 Element (p50) 9 10 ambush 10 11 magical 11 12 roll twice 12
-waiting in ambush 1 fighting/squabbling 2 prowling/on patrol 3 looking for food
-4 eating/resting 5 guarding 6 on the move 7 searching/scavenging
-8 returning to den 9 making plans 10 sleeping 11 dying 12
-alien interloper vermin lord
-criminal mastermind warlord
-high priest
-oracle wizard/witch/alchemist Monster lord (p49) evil spirit/ghost undead lord (lich, etc.) demon
-dark god
+
+*/
 */
